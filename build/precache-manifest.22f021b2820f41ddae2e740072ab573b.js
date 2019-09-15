27b87b012dd1f6b97733d79917cec5023b1b0370
Tony <korobok.avp@gmail.com>
2019-09-15 23:33:52 +0300

    добавлена папка build

---
+++ build/precache-manifest.22f021b2820f41ddae2e740072ab573b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b753299f66381ceec0b98c4457cbc7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "974e4fa3071fdc34c606",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "f65a982102ee4868fe6d",
+    "url": "/static/css/main.b5fe9779.chunk.css"
+  },
+  {
+    "revision": "974e4fa3071fdc34c606",
+    "url": "/static/js/2.a911054e.chunk.js"
+  },
+  {
+    "revision": "f65a982102ee4868fe6d",
+    "url": "/static/js/main.07768148.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
